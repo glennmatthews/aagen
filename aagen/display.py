0ed75b742e7615e61593b75507c7c8df784ea85d
Glenn Matthews <glenn@e-dad.net>
2014-01-17 11:13:27 -0500

    Simplified and improved map-to-screen calculation. Fixes #55

--- aagen/display.py
+++ aagen/display.py
@@ -58,6 +58,7 @@ class DungeonDisplay:
 
         # Provide 10' of buffer around the edges of the map
         (x0, y0, x1, y1) = (x0 - 10, y0 - 10, x1 + 10, y1 + 10)
+        log.debug("Buffered bounds are {0}".format((x0, y0, x1, y1)))
 
         # Figure out how much to scale to make the map fit the display
         map_w = (x1 - x0)
@@ -72,26 +73,27 @@ class DungeonDisplay:
         log.debug("Display scale ({0}, {1}) -> {2}"
                   .format(xscale, yscale, self.display_scale))
 
-        # Center the dungeon in the display as needed
-        # But fudge it a bit to ensure that (x0, y1) is a grid intersection
+        # Center the dungeon in the display as needed, but fudge it a bit
+        # to ensure that the origin is a grid intersection.
+        # The display uses left-handed coordinates while the map uses
+        # right-handed coordinates, so the origin for the display (its
+        # top left corner) is the x *minimum* and y *maximum* of the map.
         delta = (display_h / self.display_scale) - map_h
-        y0 = math.floor((y0 - delta/2) / 10) * 10
-        y1 = math.ceil((y1 + delta/2) / 10) * 10
+        y = math.floor((y1 + delta/2) / 10) * 10
         delta = (display_w / self.display_scale) - map_w
-        x0 = math.floor((x0 - delta/2) / 10) * 10
-        x1 = math.ceil((x1 + delta/2) / 10) * 10
+        x = math.ceil((x0 - delta/2) / 10) * 10
 
-        self.bounds = (x0, y0, x1, y1)
+        self.origin = (x, y)
 
-        log.debug("Display bounds are now {0}, scale is {1}"
-                  .format(self.bounds, self.display_scale))
+        log.debug("Map display origin is now {0}, scale is {1}"
+                  .format(self.origin, self.display_scale))
 
 
     def map_to_screen(self, coords):
         """Convert (x, y) or [(x0, y0), (x1, y1), ...] to the corresponding
         onscreen coordinates"""
 
-        (x0, y0, x1, y1) = self.bounds
+        (x0, y0) = self.origin
 
         # Note that the screen uses left-handed coordinates while the map
         # uses right-handed coordinates, so we flip the y axis when converting
@@ -102,12 +104,12 @@ class DungeonDisplay:
         if type(coords) is tuple:
             (x, y) = coords
             return ((x - x0) * self.display_scale,
-                    (y1 - y) * self.display_scale)
+                    (y0 - y) * self.display_scale)
         # Handle list of points
         output_list = []
         for (x, y) in coords:
             output_list.append(((x - x0) * self.display_scale,
-                                (y1 - y) * self.display_scale))
+                                (y0 - y) * self.display_scale))
         log.debug("map {0} corresponds to screen {1}"
                   .format(to_string(coords), to_string(output_list)))
         return output_list
@@ -115,14 +117,17 @@ class DungeonDisplay:
 
     def screen_to_map(self, coords):
         """Convert screen coordinates to the corresponding map coordinates"""
-        (x0, y0, x1, y1) = self.bounds
+        (x0, y0) = self.origin
 
         (x, y) = coords
 
         # Note that the screen uses left-handed coordinates while the map
         # uses right-handed coordinates, so we flip the y axis when converting
-        return ((x / self.display_scale) + x0,
-                -(y / self.display_scale) + y1)
+        map_coords = ((x / self.display_scale) + x0,
+                      -(y / self.display_scale) + y0)
+        log.debug("screen coords {0} --> map coords {1}"
+                  .format(coords, map_coords))
+        return map_coords
 
 
     def draw_background(self, verbosity=0):
@@ -180,22 +185,26 @@ class DungeonDisplay:
     def draw_grid(self, verbosity=0):
         """Draw the 10' grid over the dungeon background"""
         log.debug("Drawing grid")
-        (x0, y0, x1, y1) = self.bounds
+        (x0, y0) = self.origin
         color = (191, 191, 191)
 
         (w, h) = self.surface.get_size()
 
-        i = 10 * ((x0 + 9) // 10)
-        while i < x1:
+        i = x0
+        while True:
             (x, ignore) = self.map_to_screen((i, 0))
+            if x > w:
+                break
             pygame.draw.line(self.surface, color, (x, 0), (x, h), 1)
             i += 10
 
-        j = 10 * ((y0 + 9) // 10)
-        while j < y1:
+        j = y0
+        while True:
             (ignore, y) = self.map_to_screen((0, j))
+            if y > h:
+                break
             pygame.draw.line(self.surface, color, (0, y), (w, y), 1)
-            j += 10
+            j -= 10
 
 
     def draw_foreground(self, verbosity=0):
